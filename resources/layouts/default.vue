505b8ff87b86fb83a24ce3bef61cf8820702c825
ChangJoo Park <pcjpcj2@gmail.com>
2017-10-24 08:15:54 +0900

    Add app layout

--- resources/layouts/default.vue
+++ resources/layouts/default.vue
@@ -1,9 +1,72 @@
 <template>
   <v-app>
+    <!-- Drawer -->
+    <v-navigation-drawer
+      persistent
+      clipped
+      enable-resize-watcher
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-tile v-for="item in items" :key="item.text" @click="">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              {{ item.text }}
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-subheader class="mt-3 grey--text text--darken-1">SUBSCRIPTIONS</v-subheader>
+        <v-list>
+          <v-list-tile v-for="item in items2" :key="item.text" avatar @click="">
+            <v-list-tile-avatar>
+              <img :src="`https://randomuser.me/api/portraits/men/${item.picture}.jpg`" alt="">
+            </v-list-tile-avatar>
+            <v-list-tile-title v-text="item.text"></v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+        <v-list-tile class="mt-3" @click="">
+          <v-list-tile-action>
+            <v-icon color="grey darken-1">add_circle_outline</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title class="grey--text text--darken-1">Browse Projects</v-list-tile-title>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon color="grey darken-1">settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title class="grey--text text--darken-1">Manage Settings</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <!-- Toolbar -->
+    <v-toolbar color="red" dense fixed clipped-left app>
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-icon class="ml-3">fa-youtube</v-icon>
+      </v-toolbar-title>
+      <v-layout row align-center style="max-width: 650px">
+        <v-text-field
+          placeholder="Search..."
+          single-line
+          append-icon="search"
+          :append-icon-cb="() => {}"
+          class="white--text"
+          hide-details
+        ></v-text-field>
+      </v-layout>
+    </v-toolbar>
     <main>
-      <v-container fluid>
-        <nuxt/>
-      </v-container>
+      <v-content>
+        <v-container fluid>
+          <v-layout>
+            <nuxt/>
+          </v-layout>
+        </v-container>
+      </v-content>
     </main>
     <v-footer app></v-footer>
   </v-app>
@@ -16,6 +79,16 @@
     name: 'layout-default',
     components: {
       MyFooter
+    },
+    data: function () {
+      return {
+        drawer: true,
+        items: [
+          { text: 'Home', icon: 'dashboard' },
+          { text: 'About', icon: 'question_answer' }
+        ],
+        items2: []
+      }
     }
   }
 </script>
