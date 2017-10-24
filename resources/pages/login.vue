cd6c16b124f5b046dcdad926fd2fc7c0fe60a0bd
ChangJoo Park <pcjpcj2@gmail.com>
2017-10-24 23:32:53 +0900

    Add login scenarios

--- resources/pages/login.vue
+++ resources/pages/login.vue
@@ -1,14 +1,12 @@
 <template>
   <section>
-    <form>
+    <h1 class="title">Login</h1>
+    <form @submit.prevent="login">
       <div>
-        <input type="text" placeholder="Username">
+        <input type="email" placeholder="Email" v-model="email">
       </div>
       <div>
-        <input type="text" placeholder="Email">
-      </div>
-      <div>
-        <input type="text" placeholder="Password">
+        <input type="password" placeholder="Password" v-model="password">
       </div>
       <input type="submit">
     </form>
@@ -16,8 +14,33 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  layout: 'auth'
+  layout: 'auth',
+  data: function () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login: async function () {
+      try {
+        const login = await axios({
+          method: 'post',
+          url: 'http://localhost:3333/api/v1/auth',
+          data: {
+            email: this.email,
+            password: this.password
+          }
+        })
+        this.$router.push({ name: 'projects' })
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }
 }
 </script>
 
