9c08b485112a9068a86d8b22f5f9dd5944241582
ChangJoo Park <pcjpcj2@gmail.com>
2017-10-24 22:11:10 +0900

    Using username to first, last name

--- database/migrations/1503248427885_user.js
+++ database/migrations/1503248427885_user.js
@@ -6,8 +6,7 @@ class UserSchema extends Schema {
   up () {
     this.create('users', table => {
       table.increments()
-      table.string('first_name').notNullable()
-      table.string('last_name').nullable()
+      table.string('username').notNullable()
       table.string('email_address', 254).notNullable().unique()
       table.string('password', 60).notNullable()
       table.timestamps()
