5f146d4fc51204d0e93507091060ee9bb8809b8d
ChangJoo Park <pcjpcj2@gmail.com>
2017-10-24 21:53:29 +0900

    Add login / sign up link

--- resources/pages/index.vue
+++ resources/pages/index.vue
@@ -4,6 +4,10 @@
     <h1 class="title">Universal Vue.js Applications Framework</h1>
     <nuxt-link :to="{ name: 'about' }">About</nuxt-link>
     <nuxt-link :to="{ name: 'projects' }">Projects</nuxt-link>
+    <div>
+      <div><nuxt-link :to="{ name: 'login' }">Sign in</nuxt-link></div>
+      <div><nuxt-link :to="{ name: 'join' }">Sin up</nuxt-link></div>
+    </div>
   </section>
 </template>
 
