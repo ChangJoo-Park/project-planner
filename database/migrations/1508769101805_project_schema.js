0c5de35467607f73c0bbfe56274df40ca5794d23
ChangJoo Park <pcjpcj2@gmail.com>
2017-10-25 22:56:24 +0900

    Add user_id to projects schema

--- database/migrations/1508769101805_project_schema.js
+++ database/migrations/1508769101805_project_schema.js
@@ -6,6 +6,7 @@ class ProjectSchema extends Schema {
   up () {
     this.create('projects', (table) => {
       table.increments()
+      table.integer('user_id').unsigned().references('id').inTable('users')
       table.string('name').notNullable()
       table.string('description').nullable()
       table.timestamps()
