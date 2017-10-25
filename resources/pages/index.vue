b0d27e688eaa73da4334c19a7242997518e39922
ChangJoo Park <pcjpcj2@gmail.com>
2017-10-25 21:49:22 +0900

    Show links when login or not

--- resources/pages/index.vue
+++ resources/pages/index.vue
@@ -3,8 +3,10 @@
     <img src="/img/logo.png" alt="Adonuxt logo"/>
     <h1 class="title">Universal Vue.js Applications Framework</h1>
     <nuxt-link :to="{ name: 'about' }">About</nuxt-link>
-    <nuxt-link :to="{ name: 'projects' }">Projects</nuxt-link>
-    <div>
+    <div v-if="currentUser">
+      <nuxt-link :to="{ name: 'projects' }">{{currentUser.username}}</nuxt-link>
+    </div>
+    <div v-else>
       <div><nuxt-link :to="{ name: 'login' }">Sign in</nuxt-link></div>
       <div><nuxt-link :to="{ name: 'join' }">Sin up</nuxt-link></div>
     </div>
@@ -12,9 +14,14 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
   export default {
     layout: 'landing',
-    name: 'page-index'
+    name: 'page-index',
+    computed: {
+      ...mapGetters(['currentUser'])
+    }
   }
 </script>
 
