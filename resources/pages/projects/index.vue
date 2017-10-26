0eeef7bfe364124df1dacee4385a1a8ae3bdd0fa
ChangJoo Park <pcjpcj2@gmail.com>
2017-10-26 21:18:33 +0900

    Add project list and new Project link

--- resources/pages/projects/index.vue
+++ resources/pages/projects/index.vue
@@ -1,12 +1,36 @@
 <template>
   <section class="container">
-    <h1 class="title">Projects</h1>
-
-    <div v-if="emptyProjects">
-      <h2 class="title">Empty Projects!</h2>
-    </div>
-    <div v-else>
-      {{ projects }}
+    <div class="uk-child-width-1-3@s uk-grid-match" uk-grid>
+      <div v-for="project in projects" :key="project.id">
+        <div class="uk-card uk-card-default">
+            <div class="uk-card-header">
+                <div class="uk-grid-small uk-flex-middle" uk-grid>
+                    <div class="uk-width-auto">
+                        <img class="uk-border-circle" width="40" height="40" src="https://getuikit.com/docs/images/avatar.jpg">
+                    </div>
+                    <div class="uk-width-expand">
+                        <h3 class="uk-card-title uk-margin-remove-bottom">{{ project.name }}</h3>
+                        <p class="uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00">{{ project.created_at }}</time></p>
+                    </div>
+                </div>
+            </div>
+            <div class="uk-card-body">
+                <p>{{ project.description }}</p>
+            </div>
+            <div class="uk-card-footer">
+                <a href="#" class="uk-button uk-button-text">Read more</a>
+            </div>
+        </div>
+      </div>
+      <!-- New Project -->
+      <div>
+        <div class="uk-card uk-card-body">
+          <h3 class="uk-card-title"  style="text-align: center;">Start a new one!</h3>
+          <div class="uk-card-body" style="text-align: center;">
+            <nuxt-link :to="{ name: 'projects-new' }" class="uk-icon-link uk-margin-small-right" uk-icon="icon:  plus-circle; ratio: 3.5"></nuxt-link>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
