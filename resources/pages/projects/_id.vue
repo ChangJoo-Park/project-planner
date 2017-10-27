25d02089adca5818befdc62cf08ac5672de1e8ef
ChangJoo Park <pcjpcj2@gmail.com>
2017-10-27 22:04:58 +0900

    Add project detail page

---
+++ resources/pages/projects/_id.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>EEE</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  validate ({ params }) {
+    // Must be a number
+    return /^\d+$/.test(params.id)
+  }
+}
+</script>
+
+<style>
+
+</style>
