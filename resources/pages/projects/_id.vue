9ecb96c0cfe18c2711ef06ec3fc3d3e6b89be7a0
ChangJoo Park <pcjpcj2@gmail.com>
2017-10-27 22:10:20 +0900

    Add basic project details

--- resources/pages/projects/_id.vue
+++ resources/pages/projects/_id.vue
@@ -1,14 +1,24 @@
 <template>
   <div>
-    <h1>EEE</h1>
+    <h1 class="uk-text-lead">{{ project.name }}</h1>
+    <p class="uk-text-meta">{{ project.description }}</p>
+    <div>
+      {{ project }}
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   validate ({ params }) {
     // Must be a number
     return /^\d+$/.test(params.id)
+  },
+  async asyncData ({ params }) {
+    const { data: project } = await axios.get(`http://localhost:3333/api/v1/projects/${params.id}`)
+    return { project }
   }
 }
 </script>
