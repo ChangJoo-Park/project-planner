238302ee9a9eec79208a3bbdc6acc6a87b220fcd
ChangJoo Park <pcjpcj2@gmail.com>
2017-10-28 23:46:55 +0900

    Add todos to project model

--- app/Models/Project.js
+++ app/Models/Project.js
@@ -6,6 +6,10 @@ class Project extends Model {
   owner () {
     return this.belongsTo('App/Models/User')
   }
+
+  todos () {
+    return this.hasMany('App/Models/Todo')
+  }
 }
 
 module.exports = Project
