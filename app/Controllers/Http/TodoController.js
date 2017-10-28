830e16155e5be5b628b75b787c587b66766732d0
ChangJoo Park <pcjpcj2@gmail.com>
2017-10-29 00:35:44 +0900

    Add store method to TodoController

--- app/Controllers/Http/TodoController.js
+++ app/Controllers/Http/TodoController.js
@@ -1,10 +1,24 @@
 'use strict'
+const Todo = use('App/Models/Todo')
 
 class TodoController {
   async index () {
   }
 
-  async store () {
+  async store ({ request, params, auth }) {
+    const { name } = request.post()
+    const projectId = params.projects_id
+    const userId = auth.user.id
+    const newTodo = new Todo()
+    newTodo.user_id = userId
+    newTodo.project_id = projectId
+    newTodo.name = name
+    try {
+      await newTodo.save()
+      return newTodo
+    } catch (e) {
+      console.log('todo save failed')
+    }
   }
 
   async show () {
