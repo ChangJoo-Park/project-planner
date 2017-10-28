b602f2bb024046c85216099aa289d2183d6137aa
ChangJoo Park <pcjpcj2@gmail.com>
2017-10-28 23:47:09 +0900

    Add relationship belongsTo User, Project

--- app/Models/Todo.js
+++ app/Models/Todo.js
@@ -3,6 +3,13 @@
 const Model = use('Model')
 
 class Todo extends Model {
+  owner () {
+    return this.belongsTo('App/Models/User')
+  }
+
+  project () {
+    return this.belongsTo('App/Models/Project')
+  }
 }
 
 module.exports = Todo
